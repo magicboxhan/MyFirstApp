95016ed40e0e7f43e1f4e49def45d863f271ba5d
hanqing <hanqing@zhihuiya.com>
2015-03-11 16:06:37 +0800

    修改为正确的包名

--- app/src/androidTest/java/com/mycompany/myfirstapp/ApplicationTest.java
+++ app/src/androidTest/java/com/mycompany/myfirstapp/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.myconpany.myfirstapp;
+package com.mycompany.myfirstapp;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -1,4 +1,4 @@
-package com.myconpany.myfirstapp;
+package com.mycompany.myfirstapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;

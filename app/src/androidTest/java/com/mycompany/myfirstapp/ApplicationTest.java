db59bc6d73be2339c877e901c38243e4575e7f94
hanqing <hanqing@zhihuiya.com>
2015-03-13 15:55:56 +0800

    添加单元测试类 -- TestNG

--- app/src/androidTest/java/com/mycompany/myfirstapp/ApplicationTest.java
+++ app/src/androidTest/java/com/mycompany/myfirstapp/ApplicationTest.java
@@ -8,6 +8,7 @@ import android.test.ApplicationTestCase;
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
-        super(Application.class);
+        super(Application.class)
+        System.out.println("=== ApplicationTest ===");
     }
 }
---
+++ app/src/test/java/com/mycompany/myfirstapp/MyActivityTest.java
@@ -0,0 +1,18 @@
+package com.mycompany.myfirstapp;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MyActivityTest {
+
+    @Test
+    public void testSendMessage() throws Exception {
+        System.out.println("=== testSendMessage ===");
+    }
+
+    @Test
+    public void testShowWebView() throws Exception {
+        System.out.println("=== testShowWebView ===");
+    }
+}
